b2a7cd4d92582e18ef24d802279c5afa485a063a
so123nu <sonu@kreativemachinez.net>
2022-10-01 00:56:31 +0530

    app completed with payment system

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -60,7 +60,7 @@ const CartScreen = () => {
                                         <Link to={`/product/${item.product}`}>{item.name}</Link>
                                     </Col>
                                     <Col md={2}>
-                                        {item.price}
+                                        ${item.price}
                                     </Col>
                                     <Col md={2}>
                                         <Form.Control as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
